464c0c9007ed1526c8323792b197a748510aea2e
Dmitriy <d.shmyrov12@gmail.com>
2021-09-11 09:35:12 +0300

    new

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.myapplication.text.Hello;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayText();
+        for (int count = 0; count < 10; count++) {
+            Log.d("MainActivity", " counter =" + count);
+        }
+
+    }
+    private void displayText(){
+        Hello h = new Hello();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(h.getValue());
     }
 }
